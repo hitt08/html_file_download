a08a0db7dfc33637b85c51ad45276c03570a703f
Hitarth Narvala <44180325+hitt08@users.noreply.github.com>
2020-07-29 17:51:07 +0100

    Update download_file.sh

--- download_file.sh
+++ download_file.sh
@@ -24,7 +24,7 @@ while [[ "$#" -gt 0 ]];do
       ;;
     -h|--help)
       BREAK=1
-      echo "download_file.sh [OPTION]"
+      echo "$1 [OPTION]"
           echo "  -p, --pattern   File Pattern, Default: ${pattern}"
           echo "  -u|--url        Web URL"
           echo "  -f|--file       File with list of WebURL[,OutputFileName] (separated in each line)"
@@ -37,31 +37,36 @@ done
 if [ $BREAK -eq 0 ];then
         if [[ ! -z $URL ]];then
                 vid_url=`curl -L $URL 2>/dev/null |awk -v var=$pattern -F "$pattern" '{print $1 var}' | awk -F '="' '{print $NF}' | tail -1`
-                res="Downloading "$vid_url
-                if [[ ! -z $NAME ]];then
-                                n='-O '$NAME
-                        res=$res" to file "$NAME
+                if [[ -z $vid_url ]];then
+                        echo "*."$pattern" Content not found"
                 else
-                                n=''
-                fi
-                echo $res
-                wget $vid_url $n 2>/dev/null
-        fi
-        if [[ ! -z $URL_FILE ]];then
-                while read line; do
-                        url=`echo $line | cut -d "," -f1`
-                        name=`echo $line | cut -d "," -f2`
-
-                        vid_url=`curl -L $url 2>/dev/null |awk -v var=$pattern -F "$pattern" '{print $1 var}' | awk -F '="' '{print $NF}' | tail -1`
                         res="Downloading "$vid_url
-                        if [[ ! -z $name ]];then
-                                        n='-O '$name
-                                res=$res" to file "$name
+                        if [[ ! -z $NAME ]];then
+                                n='-O '$NAME.$pattern
+                                res=$res" to file "$NAME.$pattern
                         else
-                                        n=''
+                                n=''
                         fi
                         echo $res
                         wget $vid_url $n 2>/dev/null
+                fi
+        fi
+        if [[ ! -z $URL_FILE ]];then
+                while read line; do
+                        url=`echo $line | awk -F "," '{print $1}'`
+                        name=`echo $line | awk -F "," '{print $2}'`
+                        vid_url=`curl -L $url 2>/dev/null |awk -v var=$pattern -F "$pattern" '{print $1 var}' | awk -F '="' '{print $NF}' | tail -1`
+                        if [[ ! -z $vid_url ]];then
+                                res="Downloading "$vid_url
+                                if [[ ! -z $name ]];then
+                                        n='-O '$name.$pattern
+                                        res=$res" to file "$name.$pattern
+                                else
+                                        n=''
+                                fi
+                                echo $res
+                                wget $vid_url $n 2>/dev/null
+                        fi
                 done < $URL_FILE
         fi
 fi
